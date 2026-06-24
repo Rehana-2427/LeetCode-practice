30beab6cb894b2f996a031ae599a769e9daa0fd7
Shaik rehana <106529695+Rehana-2427@users.noreply.github.com>
2026-06-25 00:52:44 +0530

    Optimize power of two check loop
    Optimized the loop to check for power of two by limiting iterations to 30.

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -6,6 +6,8 @@ class Solution {
         if(n==1){
             return true;
         }
+         // Optimized Loop: Instead of looping up to 'n' (which causes timeouts),
+        // we only loop up to 30 because 2^31 overflows a standard Java int.
         for(int i=1;i<=30;i++){
             if(n==Math.pow(2,i)){
                 return true;
