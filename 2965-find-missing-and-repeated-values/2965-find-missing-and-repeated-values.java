edc92278947b82f2d5164f94a322f2cb37f0a1b9
Shaik rehana <106529695+Rehana-2427@users.noreply.github.com>
2026-07-09 21:39:42 +0530

    Time: 1 ms (99.98%), Space: 47.2 MB (75.68%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -1,17 +1,25 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int[] flatArray = java.util.Arrays.stream(grid)
-                        .flatMapToInt(java.util.Arrays::stream)
-                        .toArray();
-        int n = flatArray.length;
+        // int[] flatArray = java.util.Arrays.stream(grid)
+        //                 .flatMapToInt(java.util.Arrays::stream)
+        //                 .toArray();
+        // int n = flatArray.length;
 
-        int[] freq = new int[n+1];
-        for(int i=0;i<n;i++){
-            freq[flatArray[i]]++;
+        int n = grid.length;
+        int total = n * n;
+
+        int[] freq = new int[total + 1];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                freq[grid[i][j]]++;
+            }
         }
+
+    
         int repeated = 0;
         int missed = 0;
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=total;i++){
             if(freq[i]==0){
                 missed = i;
             }
