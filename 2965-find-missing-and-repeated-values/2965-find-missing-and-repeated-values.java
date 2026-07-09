ea1de5e128d719cebea91a8696c3190a2d8ee0d9
Shaik rehana <106529695+Rehana-2427@users.noreply.github.com>
2026-07-09 21:43:12 +0530

    Time: 1 ms (99.98%), Space: 47.3 MB (59.02%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -1,38 +1,69 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        // int[] flatArray = java.util.Arrays.stream(grid)
-        //                 .flatMapToInt(java.util.Arrays::stream)
-        //                 .toArray();
-        // int n = flatArray.length;
 
         int n = grid.length;
+
+        // ---------------- Frequency Array Approach ----------------
+
+        // int total = n * n;
+        // int[] freq = new int[total + 1];
+
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         freq[grid[i][j]]++;
+        //     }
+        // }
+
+        // int repeated = 0;
+        // int missing = 0;
+
+        // for (int i = 1; i <= total; i++) {
+        //     if (freq[i] == 2)
+        //         repeated = i;
+        //     else if (freq[i] == 0)
+        //         missing = i;
+        // }
+
+        // return new int[]{repeated, missing};
+
+        // ---------------- Math Formula Approach ----------------
+
         int total = n * n;
 
-        int[] freq = new int[total + 1];
+        long expectedSum = (long) total * (total + 1) / 2;
+        long expectedSquareSum = (long) total * (total + 1) * (2L * total + 1) / 6;
+
+        long actualSum = 0;
+        long actualSquareSum = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                freq[grid[i][j]]++;
+
+                int value = grid[i][j];
+
+                actualSum += value;
+                actualSquareSum += (long) value * value;
             }
         }
 
-    
-        int repeated = 0;
-        int missed = 0;
-        for(int i=1;i<=total;i++){
-            if(freq[i]==0){
-                missed = i;
-            }
-            else if(freq[i] == 2){
-                repeated = i;
-            }
-        }
-        int[] result = new int[2];
-        for(int i=0;i<2;i++){
-            result[0]=repeated;
-            result[1] = missed;
-        }
-        return result;
+        // x = repeated
+        // y = missing
 
+        // x - y
+        long diff = actualSum - expectedSum;
+
+        // x² - y²
+        long squareDiff = actualSquareSum - expectedSquareSum;
+
+        // (x²-y²)/(x-y) = x+y
+        long sum = squareDiff / diff;
+
+        // x = ((x+y)+(x-y))/2
+        int repeated = (int) ((sum + diff) / 2);
+
+        // y = x-(x-y)
+        int missing = (int) (repeated - diff);
+
+        return new int[]{repeated, missing};
     }
 }
