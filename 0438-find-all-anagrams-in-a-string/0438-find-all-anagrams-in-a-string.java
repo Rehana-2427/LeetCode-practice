04b46a6c1159c100356614074a19be7ac6373d0d
Shaik rehana <106529695+Rehana-2427@users.noreply.github.com>
2026-07-30 23:53:16 +0530

    Time: 32 ms (29.04%), Space: 47 MB (46.52%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,56 +1,43 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
 
-        List<Integer> result = new ArrayList<>();
-
-        if (s.length() < p.length()) {
-            return result;
-        }
-
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        // Frequency map of p
-        for (char ch : p.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-
         int left = 0;
         int right = 0;
         int count = p.length();
-
-        while (right < s.length()) {
-
-            char rightChar = s.charAt(right);
-
-            if (map.containsKey(rightChar)) {
-                if (map.get(rightChar) > 0) {
+        List<Integer> result = new ArrayList<>();
+        if(s.length()<p.length()){
+            return result;
+        }
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char c:p.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        while(right<s.length()){
+            char  rightChar = s.charAt(right);
+            if(map.containsKey(rightChar)){
+                if(map.get(rightChar)>0){
                     count--;
                 }
-                map.put(rightChar, map.get(rightChar) - 1);
+                map.put(rightChar,map.get(rightChar)-1);
             }
-
             right++;
 
-            if (count == 0) {
+            if(count==0){
                 result.add(left);
             }
 
-            // Maintain fixed window
-            if (right - left == p.length()) {
-
+            if(right-left==p.length()){
                 char leftChar = s.charAt(left);
-
-                if (map.containsKey(leftChar)) {
-                    if (map.get(leftChar) >= 0) {
+                if(map.containsKey(leftChar)){
+                    if(map.get(leftChar)>=0){
                         count++;
                     }
-                    map.put(leftChar, map.get(leftChar) + 1);
+                    map.put(leftChar,map.get(leftChar)+1);
                 }
-
                 left++;
             }
-        }
 
+        }
         return result;
     }
 }
